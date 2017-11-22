e8cbc00b2c9fa785fbe8eca268835d78e557e008
shiyangyang <shiyangyang@flowpp.com>
2017-11-22 17:51:30 +0800

    v0.1.0

--- .f2econfig.js
+++ .f2econfig.js
@@ -1,14 +1,16 @@
 const { argv } = process
 const build = argv[argv.length - 1] === 'build'
+const getModuleId = pathname => pathname.replace('src/', '')
 const { onRoute } = require('./serve/index')
 module.exports = {
+    port: 5555,
     livereload: !build,
     build,
     gzip: true,
-    // app: 'static',
     middlewares: [
         {
-            middleware: 'template'
+            middleware: 'template',
+            test: /index\.html|require/
         },
         {
             middleware: 'typescript'
--- src/type.js
+++ src/type.js
@@ -1,4 +1,4 @@
-define([
+define('type', [
     'exports'
 ], function(exports) {
     'use strict';
